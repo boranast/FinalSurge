5af31b3d9633cc26425028f797c37d7076f717d9
Anastasiya <nastya.bor.002@mail.ru>
2022-05-26 21:35:53 +0300

    Added new tests

---
+++ src/test/java/tests/WorkoutTest.java
@@ -0,0 +1,195 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class WorkoutTest extends BaseTest{
+
+    @Test
+    public void addRunWorkout() {
+        loginPage.open();
+        loginPage.login(USER, PASSWORD);
+        workoutPage.open();
+        driver.findElement(By.cssSelector("[data-code=run]")).click();
+        driver.findElement(By.id("WorkoutTime")).sendKeys("16:30");
+        driver.findElement(By.id("Name")).sendKeys("Run");
+        driver.findElement(By.id("Desc")).sendKeys("Running in the park");
+        driver.findElement(By.id("Distance")).sendKeys("3000");
+        driver.findElement(By.id("Duration")).sendKeys("03:19:06");
+        driver.findElement(By.id("Pace")).sendKeys("03:19");
+        Select select1 = new Select(driver.findElement(By.id("DistTypeNoInt")));
+        select1.selectByVisibleText("min/100m");
+        assertTrue(select1.getOptions().get(4).isSelected());
+        driver.findElement(By.id("hf_great")).click();
+
+        Select select2 = new Select(driver.findElement(By.id("PerEffort")));
+        select2.selectByVisibleText("2 (Light)");
+        assertTrue(select2.getOptions().get(2).isSelected());
+
+        driver.findElement(By.id("MinHR")).sendKeys("10");
+        driver.findElement(By.id("AvgHR")).sendKeys("10");
+        driver.findElement(By.id("MaxHR")).sendKeys("10");
+        driver.findElement(By.id("kCal")).sendKeys("500");
+        driver.findElement(By.id("saveButton")).click();
+
+    }
+
+    @Test
+    public void addSwimWorkout() {
+        loginPage.open();
+        loginPage.login(USER, PASSWORD);
+        workoutPage.open();
+        driver.findElement(By.cssSelector("[data-code=swim]")).click();
+        driver.findElement(By.id("WorkoutTime")).sendKeys("16:30");
+        driver.findElement(By.id("Name")).sendKeys("Swim");
+        driver.findElement(By.id("Desc")).sendKeys("Swimming in the pool");
+        driver.findElement(By.id("tIntervals")).click();
+        driver.findElement(By.id("SetReps1")).sendKeys("4");
+        driver.findElement(By.id("SetDist1")).sendKeys("400");
+        driver.findElement(By.id("SetTime1")).sendKeys("20:00");
+        driver.findElement(By.id("SetAdd1")).click();
+        driver.findElement(By.id("SetRestTime1")).sendKeys("05:00");
+        driver.findElement(By.id("SetRestAmount1")).sendKeys("300");
+
+        driver.findElement(By.id("SetReps2")).sendKeys("4");
+        driver.findElement(By.id("SetDist2")).sendKeys("400");
+        driver.findElement(By.id("SetTime2")).sendKeys("20:00");
+
+        driver.findElement(By.id("hf_good")).click();
+        Select select = new Select(driver.findElement(By.id("PerEffort")));
+        select.selectByVisibleText("1 (Very Light)");
+        assertTrue(select.getOptions().get(1).isSelected());
+
+        driver.findElement(By.id("kCal")).sendKeys("800");
+        driver.findElement(By.id("saveButton")).click();
+
+    }
+
+    @Test
+    public void addCrossTrainingWorkout() {
+        loginPage.open();
+        loginPage.login(USER, PASSWORD);
+        workoutPage.open();
+        driver.findElement(By.cssSelector("[data-code=cross-trai]")).click();
+        driver.findElement(By.id("WorkoutTime")).sendKeys("16:30");
+        driver.findElement(By.id("Name")).sendKeys("Cross");
+        driver.findElement(By.id("Desc")).sendKeys("Cross in the park");
+
+        driver.findElement(By.id("DistanceNoInt")).sendKeys("3500");
+        Select select1 = new Select(driver.findElement(By.id("DistTypeNoInt")));
+        select1.selectByVisibleText("m");
+        assertTrue(select1.getOptions().get(2).isSelected());
+
+        driver.findElement(By.id("DurationNoInt")).sendKeys("00:10:00");
+
+        driver.findElement(By.id("hf_normal")).click();
+        Select select2 = new Select(driver.findElement(By.id("PerEffort")));
+        select2.selectByVisibleText("3 (Light)");
+        assertTrue(select2.getOptions().get(3).isSelected());
+
+        driver.findElement(By.id("MinHR")).sendKeys("100");
+        driver.findElement(By.id("AvgHR")).sendKeys("120");
+        driver.findElement(By.id("MaxHR")).sendKeys("150");
+        driver.findElement(By.id("kCal")).sendKeys("800");
+        driver.findElement(By.id("saveButton")).click();
+
+    }
+
+    @Test
+    public void addWalkWorkout() {
+        loginPage.open();
+        loginPage.login(USER, PASSWORD);
+        workoutPage.open();
+        driver.findElement(By.cssSelector("[data-code=walk]")).click();
+        driver.findElement(By.id("WorkoutDate")).clear();
+        driver.findElement(By.id("WorkoutDate")).sendKeys("5/25/2022");
+        driver.findElement(By.id("WorkoutTime")).sendKeys("16:30");
+        driver.findElement(By.id("Name")).sendKeys("Walk");
+        driver.findElement(By.id("Desc")).sendKeys("Walk on the streets");
+
+        driver.findElement(By.id("Distance")).sendKeys("5500");
+        Select select1 = new Select(driver.findElement(By.id("DistType")));
+        select1.selectByVisibleText("m");
+        assertTrue(select1.getOptions().get(2).isSelected());
+
+        driver.findElement(By.id("Duration")).sendKeys("00:10:00");
+        driver.findElement(By.id("EGain")).sendKeys("6000");
+        driver.findElement(By.id("ELoss")).sendKeys("500");
+
+
+        driver.findElement(By.id("hf_normal")).click();
+        Select select2 = new Select(driver.findElement(By.id("PerEffort")));
+        select2.selectByVisibleText("3 (Light)");
+        assertTrue(select2.getOptions().get(3).isSelected());
+
+        driver.findElement(By.id("PowAvg")).sendKeys("100");
+        driver.findElement(By.id("PowMax")).sendKeys("200");
+        driver.findElement(By.id("CadAvg")).sendKeys("120");
+        driver.findElement(By.id("CadMax")).sendKeys("150");
+        driver.findElement(By.id("MinHR")).sendKeys("100");
+        driver.findElement(By.id("AvgHR")).sendKeys("120");
+        driver.findElement(By.id("MaxHR")).sendKeys("150");
+        driver.findElement(By.id("kCal")).sendKeys("800");
+        driver.findElement(By.id("saveButton")).click();
+
+    }
+
+    @Test
+    public void addRestDayWorkout() {
+        loginPage.open();
+        loginPage.login(USER, PASSWORD);
+        workoutPage.open();
+        driver.findElement(By.cssSelector("[data-code=rest]")).click();
+        driver.findElement(By.id("WorkoutDate")).clear();
+        driver.findElement(By.id("WorkoutDate")).sendKeys("5/24/2022");
+
+        driver.findElement(By.id("Name")).sendKeys("Rest");
+        driver.findElement(By.id("Desc")).sendKeys("Resting");
+
+        driver.findElement(By.id("saveButton")).click();
+
+    }
+
+    @Test
+    public void addStrengthTrainingWorkout() {
+        loginPage.open();
+        loginPage.login(USER, PASSWORD);
+        workoutPage.open();
+        driver.findElement(By.cssSelector("[data-code=strength-t]")).click();
+        driver.findElement(By.id("WorkoutDate")).clear();
+        driver.findElement(By.id("WorkoutDate")).sendKeys("5/23/2022");
+        driver.findElement(By.id("WorkoutTime")).sendKeys("16:30");
+        driver.findElement(By.id("Name")).sendKeys("Strength Training");
+        driver.findElement(By.id("Desc")).sendKeys("Strength Training in the gym");
+
+        driver.findElement(By.id("DurationNoInt")).sendKeys("01:50:00");
+
+        driver.findElement(By.id("hf_great")).click();
+        Select select2 = new Select(driver.findElement(By.id("PerEffort")));
+        select2.selectByVisibleText("1 (Very Light)");
+        assertTrue(select2.getOptions().get(1).isSelected());
+
+        driver.findElement(By.id("saveButton")).click();
+
+    }
+
+    @Test
+    public void addRecoveryWorkout() {
+        loginPage.open();
+        loginPage.login(USER, PASSWORD);
+        workoutPage.open();
+        driver.findElement(By.cssSelector("[data-code=recovery]")).click();
+        driver.findElement(By.id("WorkoutDate")).clear();
+        driver.findElement(By.id("WorkoutDate")).sendKeys("5/22/2022");
+        driver.findElement(By.id("WorkoutTime")).sendKeys("16:30");
+        driver.findElement(By.id("Name")).sendKeys("Recovery");
+        driver.findElement(By.id("Desc")).sendKeys("Recovery");
+
+        driver.findElement(By.id("saveButton")).click();
+
+    }
+
+}
